409929ef9bdbd6d3e16cedcf0f24d15bd8119116
Arnaud Panapadeatchy <arnaud.panapadeatchy@dsinstruments.fr>
2021-11-23 11:32:26 +0400

    Add a hero character on the demo map

--- Overworld.js
+++ Overworld.js
@@ -11,5 +11,40 @@ class Overworld {
       this.ctx.drawImage(image, 0, 0);
     };
     image.src = "/images/maps/DemoLower.png";
+
+    const x = 5;
+    const y = 6;
+
+    const shadow = new Image();
+    shadow.onload = () => {
+      this.ctx.drawImage(
+        shadow, 
+        0, // left cut,
+        0, // top cut
+        32, // width of cut
+        32, // height of cut
+        x * 16 - 8,
+        y * 16 - 18,
+        32,
+        32
+      );
+    };
+    shadow.src = "/images/characters/shadow.png";
+
+    const hero = new Image();
+    hero.onload = () => {
+      this.ctx.drawImage(
+        hero, 
+        0, // left cut,
+        0, // top cut
+        32, // width of cut
+        32, // height of cut
+        x * 16 - 8,
+        y * 16 - 18,
+        32,
+        32
+      );
+    };
+    hero.src = "/images/characters/people/hero.png";
   }
 }

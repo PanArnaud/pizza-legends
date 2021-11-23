2e4dae0f584ad1e7d9d4049d0deb972de28158cf
Arnaud Panapadeatchy <arnaud.panapadeatchy@dsinstruments.fr>
2021-11-23 15:42:53 +0400

    Move character

--- Overworld.js
+++ Overworld.js
@@ -3,30 +3,35 @@ class Overworld {
     this.element = config.element;
     this.canvas = config.element.querySelector('.game-canvas');
     this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+  }
+
+  startGameLoop() {
+    const step = () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.map.drawLowerImage(this.ctx);
+
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.update({
+          arrow: this.directionInput.direction
+        });
+        object.sprite.draw(this.ctx);
+      });
+
+      this.map.drawUpperImage(this.ctx);
+      requestAnimationFrame(() => {
+        step();
+      });
+    };
+    step();
   }
 
   init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = "/images/maps/DemoLower.png";
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
 
-    // Place somes game objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6
-    });
-
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: "/images/characters/people/npc1.png"
-    });
-
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
+    this.startGameLoop();
   }
 }
---
+++ OverworldMap.js
@@ -0,0 +1,59 @@
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
+
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
+
+  drawLowerImage(ctx) {
+    ctx.drawImage(this.lowerImage, 0, 0);
+  }
+
+  drawUpperImage(ctx) {
+    ctx.drawImage(this.upperImage, 0, 0);
+  }
+}
+
+window.OverworldMaps = {
+  DemoRoom: {
+    lowerSrc: "/images/maps/DemoLower.png",
+    upperSrc: "/images/maps/DemoUpper.png",
+    gameObjects: {
+      hero: new Person({
+        isPlayerControlled: true,
+        x: utils.withGrid(5),
+        y: utils.withGrid(6)
+      }),
+      npc1: new Person({
+        x: utils.withGrid(7),
+        y: utils.withGrid(9),
+        src: "/images/characters/people/npc1.png"
+      })
+    }
+  },
+  Kitchen: {
+    lowerSrc: "/images/maps/KitchenLower.png",
+    upperSrc: "/images/maps/KitchenUpper.png",
+    gameObjects: {
+      hero: new Person({
+        isPlayerControlled: true,
+        x: utils.withGrid(3),
+        y: utils.withGrid(5)
+      }),
+      npc1: new Person({
+        x: utils.withGrid(9),
+        y: utils.withGrid(6),
+        src: "/images/characters/people/npc1.png"
+      }),
+      npc2: new Person({
+        x: utils.withGrid(10),
+        y: utils.withGrid(8),
+        src: "/images/characters/people/npc2.png"
+      })
+    }
+  },
+}

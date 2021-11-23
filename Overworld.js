922ae3728509f591717363dc1c985b5ad60c5c2f
Arnaud Panapadeatchy <arnaud.panapadeatchy@dsinstruments.fr>
2021-11-23 16:38:33 +0400

    Center camera on player

--- Overworld.js
+++ Overworld.js
@@ -10,16 +10,22 @@ class Overworld {
     const step = () => {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      this.map.drawLowerImage(this.ctx);
+      // Establish the camera person
+      const cameraPerson = this.map.gameObjects.hero;
 
       Object.values(this.map.gameObjects).forEach(object => {
         object.update({
           arrow: this.directionInput.direction
         });
-        object.sprite.draw(this.ctx);
       });
 
-      this.map.drawUpperImage(this.ctx);
+      this.map.drawLowerImage(this.ctx, cameraPerson);
+
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.sprite.draw(this.ctx, cameraPerson);
+      });
+
+      this.map.drawUpperImage(this.ctx, cameraPerson);
       requestAnimationFrame(() => {
         step();
       });
--- Sprite.js
+++ Sprite.js
@@ -28,7 +28,7 @@ class Sprite {
       "walk-up": [ [1, 2], [0, 2], [3, 2],  [0, 2] ],
       "walk-left": [ [1, 3], [0, 3], [3, 3],  [0, 3] ]
     };
-    this.currentAnimation = "walk-down" // config.currentAnimation || "idle-down";
+    this.currentAnimation = "idle-down" // config.currentAnimation || "idle-down";
     this.currentAnimationFrame = 0;
 
     this.animationFrameLimit = config.animationFrameLimit || 8;
@@ -64,9 +64,9 @@ class Sprite {
     }
   }
 
-  draw(ctx) {
-    const x = this.gameObject.x - 8;
-    const y = this.gameObject.y - 18;
+  draw(ctx, cameraPerson) {
+    const x = this.gameObject.x - 8 + utils.withGrid(10.5) - cameraPerson.x;
+    const y = this.gameObject.y - 18 + utils.withGrid(6) - cameraPerson.y;
 
     this.isShadowLoaded && ctx.drawImage(
       this.shadow,

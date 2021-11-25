a46cfca26bb838f664ec1423ef7605da3ab18e78
PanArnaud <a.panapadeatchy@hotmail.fr>
2021-11-26 00:32:06 +0400

    End first episode for battle'

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -1,15 +1,61 @@
 class Battle {
-  constructor() {}
+  constructor() {
+    this.combatants = {
+      player1: new Combatant(
+        {
+          ...Pizzas.s001,
+          team: "player",
+          hp: 50,
+          maxHp: 50,
+          xp: 75,
+          maxXp: 100,
+          level: 1,
+          status: null,
+        },
+        this
+      ),
+      enemy1: new Combatant(
+        {
+          ...Pizzas.v001,
+          team: "enemy",
+          hp: 50,
+          maxHp: 50,
+          xp: 2,
+          maxXp: 100,
+          level: 1,
+          status: null,
+        },
+        this
+      ),
+      enemy2: new Combatant(
+        {
+          ...Pizzas.f001,
+          team: "enemy",
+          hp: 50,
+          maxHp: 50,
+          xp: 30,
+          maxXp: 100,
+          level: 1,
+          status: null,
+        },
+        this
+      ),
+    };
+    this.activeCombatants = {
+      player: "player1",
+      enemy: "enemy1"
+    };
+  }
 
   createElement() {
     this.element = document.createElement("div");
     this.element.classList.add("Battle");
     this.element.innerHTML = `
       <div class="Battle_hero">
-        <img src="${'/images/characters/people/hero.png'}" alt="Hero" />
+        <img src="${"/images/characters/people/hero.png"}" alt="Hero" />
       </div>
       <div class="Battle_enemy">
-        <img src="${'/images/characters/people/npc3.png'}" alt="Enemy" />
+        <img src="${"/images/characters/people/npc3.png"}" alt="Enemy" />
       </div>
     `;
   }
@@ -17,5 +63,11 @@ class Battle {
   init(container) {
     this.createElement();
     container.appendChild(this.element);
+
+    Object.keys(this.combatants).forEach(key => {
+      let combatant = this.combatants[key];
+      combatant.id = key;
+      combatant.init(this.element);
+    })
   }
 }
